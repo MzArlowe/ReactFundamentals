1e17355c7666740a7f97733f5f1889810444d4a4
unknown <bhm727@yahoo.com>
2021-11-15 00:35:44 -0500

    Update Sidebar

--- src/components/site/Sidebar.js
+++ src/components/site/Sidebar.js
@@ -6,15 +6,19 @@ import {
 import Home from './Home';
 import Resources from './Resources';
 import FunctionalComponentsDemo from '../concepts/FunctionalComponentsDemo/FunctionalComponentsDemo';
+import JSXRules from '../concepts/JSXRules';
+import State from '../concepts/State';
 
 const Sidebar = () => {
-    return(
+    return (
         <div className='sidebar'>
             <div className='sidebar-list-styling'>
                 <ul className='sidebar-list list-unstyled'>
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to='/resources'>Resources</Link></li>
                     <li><Link to='/functionalcomponentsdemo'>Functional Component</Link></li>
+                    <li><Link to='/jsxrules'>JSX Rules</Link></li>
+                    <li><Link to='/state'>State</Link></li>
                 </ul>
             </div>
             <div className='sidebar-route'>
@@ -23,6 +27,8 @@ const Sidebar = () => {
                     <Route exact path='/resources'><Resources /></Route>
                     <Route exact path='/'><Home /></Route>
                     <Route exact path='/functionalcomponentsdemo'><FunctionalComponentsDemo /></Route>
+                    <Route exact path='/jsxrules'><JSXRules /></Route>
+                    <Route exact path='/state'><State /></Route>
                 </Switch>
             </div>
         </div>
